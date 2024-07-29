adf1ffa0b24dfeaf09d00125c17530a28416c9ec
kalyan9850 <kalyanaute7@gmai.com>
2024-07-29 16:02:42 +0530

    first commit

---
+++ capital.js
@@ -0,0 +1,19 @@
+// let names = 'kalyan';
+let names='ramanand'
+
+let length= names.length;
+
+if(length==0){
+    console.log(names)
+}
+else if(length==1){
+    console.log(names.toUpperCase());
+}
+else{
+    let midd=Math.floor(length/2);
+    let op=names.slice(0,midd)+names[midd].toUpperCase()+names.slice(midd+1);
+    console.log(op);
+}
+
+
+
